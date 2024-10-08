95fc7ce67beb3eb8e051cf03ecc501cf56b086c0
LastS3cond <jaydon.n.bingham@gmail.com>
2024-10-08 15:23:30 -0400

    Changed chart design

--- odysseus-web/src/components/SectionsCharts.tsx
+++ odysseus-web/src/components/SectionsCharts.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { Section } from "@prisma/client";
 import { Table } from '@mantine/core';
-import { AreaChart } from "@mantine/charts";
+import { AreaChart, ChartTooltipProps, getFilteredChartTooltipPayload } from "@mantine/charts";
 import '@mantine/charts/styles.css';
+import { Paper, Text } from '@mantine/core';
 
 interface SectionsGraphProps {
   sections: Section[]
@@ -30,6 +31,22 @@ type CombinedProfs = {
   F: number[];
   W: number[];
 }
+function ChartTooltip({ label, payload }: ChartTooltipProps) {
+  if (!payload) return null;
+  const reversedPayload = [...payload].reverse();
+  return (
+    <Paper px="md" py="sm" withBorder shadow="md" radius="md">
+      <Text fw={500} mb={5}>
+        {label}
+      </Text>
+      {getFilteredChartTooltipPayload(reversedPayload).map((item: any) => (
+        <Text key={item.name} c={item.color} fz="sm">
+          {item.name}: {Math.round(item.value / item.payload.avgGPA * 1000) / 10}%
+        </Text>
+      ))}
+    </Paper>
+  );
+}
 const adjustSections = (prev: number, n: number, num: number) => {
   return ((prev * num) + n) / (num + 1)
 }
@@ -41,6 +58,7 @@ export function SectionsGraph({ sections }: SectionsGraphProps) {
       _key: Number(section.year + "" + terms[section.term as keyof typeof terms]),
       term: `${section.term} ${section.year}`,
       numberSections: 1,
+      avgGPA: section.gpa,
       A: gradeCount[0] + gradeCount[1],
       B: gradeCount[2] + gradeCount[3] + gradeCount[4],
       C: gradeCount[5] + gradeCount[6] + gradeCount[7],
@@ -52,7 +70,7 @@ export function SectionsGraph({ sections }: SectionsGraphProps) {
     if (!acc[curr.term]) {
       acc[curr.term] = { ...curr };
     } else {
-
+      acc[curr.term].avgGPA = adjustSections(acc[curr.term].avgGPA, curr.avgGPA, acc[curr.term].numberSections);
       acc[curr.term].A = adjustSections(acc[curr.term].A, curr.A, acc[curr.term].numberSections);
       acc[curr.term].B = adjustSections(acc[curr.term].B, curr.B, acc[curr.term].numberSections);
       acc[curr.term].C = adjustSections(acc[curr.term].C, curr.C, acc[curr.term].numberSections);
@@ -64,23 +82,29 @@ export function SectionsGraph({ sections }: SectionsGraphProps) {
     return acc;
   }, {} as Record<string, CombinedGrades>)).sort((a, b) => a._key - b._key);
   termData.forEach((currTerm, index) => {
-    termData[index].A = Math.round(currTerm.A * 10) / 10
-    termData[index].B = Math.round(currTerm.B * 10) / 10
-    termData[index].C = Math.round(currTerm.C * 10) / 10
-    termData[index].D = Math.round(currTerm.D * 10) / 10
-    termData[index].F = Math.round(currTerm.F * 10) / 10
-    termData[index].W = Math.round(currTerm.W * 10) / 10
+    termData[index].avgGPA = Math.round(currTerm.avgGPA * 100) / 100
+    termData[index].A = (currTerm.A * 10) / 10 * termData[index].avgGPA / 100
+    termData[index].B = (currTerm.B * 10) / 10 * termData[index].avgGPA / 100
+    termData[index].C = (currTerm.C * 10) / 10 * termData[index].avgGPA / 100
+    termData[index].D = (currTerm.D * 10) / 10 * termData[index].avgGPA / 100
+    termData[index].F = (currTerm.F * 10) / 10 * termData[index].avgGPA / 100
+    termData[index].W = (currTerm.W * 10) / 10 * termData[index].avgGPA / 100
   })
 
   return (
-    <AreaChart h={300} data={termData} dataKey="term" type="stacked" series={[
-      { name: 'W', color: 'black' },
-      { name: 'F', color: 'red.6' },
-      { name: 'D', color: 'orange.6' },
-      { name: 'C', color: 'yellow.6' },
-      { name: 'B', color: 'green.6' },
-      { name: 'A', color: 'cyan.6' },
-    ]} />
+    <AreaChart h={300} data={termData} dataKey="term" type="stacked"
+      tooltipProps={{
+        content: ({ label, payload }) => <ChartTooltip label={label} payload={payload} />,
+      }}
+      curveType="step"
+      series={[
+        { name: 'W', color: 'black' },
+        { name: 'F', color: 'red.6' },
+        { name: 'D', color: 'orange.6' },
+        { name: 'C', color: 'yellow.6' },
+        { name: 'B', color: 'green.6' },
+        { name: 'A', color: 'cyan.6' },
+      ]} />
   )
 }
 
