dd2f8f279fc0ac49e4e398d8f115feafa0697d03
LastS3cond <jaydon.n.bingham@gmail.com>
2025-04-24 16:44:42 -0400

    Updated data scraping to account for deleted classes and iterate till the end

--- data/data-utils/async_get_classes.py
+++ data/data-utils/async_get_classes.py
@@ -4,38 +4,71 @@ import os
 import json
 
 # Create a global list to store the class data
-classes = []
+inProgess = True
 
-
-async def fetch(session, i):
+async def fetch(session, i, rawClasses, newClasses):
+    global inProgess
     try:
         async with session.post(
             url="https://catalog.vt.edu/course-search/api/?page=fose&route=details",
             data=f'{{"group":"key:{i}"}}',
         ) as res:
             data = await res.json()
-            classes.append(data)
-            print(len(classes))
+            if data == []:
+                inProgess = False;
+                raise Exception("No data")
+            prevEntry = None
+            prevIndex = -1
+            for index, entry in enumerate(rawClasses):
+                if entry["code"] == data['code']:
+                    prevEntry = entry
+                    prevIndex = index
+                    break
+
+            if not prevEntry:
+                newClasses.append(data)
+                print(f"New class data {data['code']}")
+            else: 
+                dict1_compare = {k: v for k, v in prevEntry.items() if k != 'key' and k != 'allInGroup'}
+                dict2_compare = {k: v for k, v in data.items() if k != 'key' and k != 'allInGroup'}
+                
+                if dict1_compare != dict2_compare:
+                    print(f"Updated class data {data['code']}")
+                newClasses.append(data)
+                rawClasses.pop(prevIndex)
+                
+
     except Exception as e:
         print(f"An error occurred for i={i}: {e}")
 
 
 async def main():
-    if os.path.exists("../raw-data/classes.json"):
-        os.remove("../raw-data/classes.json")
+    rawClasses = []
+    if os.path.exists("../raw-data/rawClasses.json"):
+        with open("../raw-data/rawClasses.json", "r") as jsonFile:
+            rawClasses = json.loads(jsonFile.read())
 
+    newClasses = []
     async with aiohttp.ClientSession() as session:
         step = 10
-        max = 8223
-        for i in range(1, max, step):
+        i = 1
+        while (inProgess):
             await asyncio.gather(
-                *[fetch(session, i + j) for j in range(step) if i + j < max]
+                *[fetch(session, i + j, rawClasses, newClasses) for j in range(step)]
             )
-            print("sleeping")
+            i += 10
+            if (i-1) % 100 == 0: 
+                print(f"----------{i-1} requests sent.----------")
             await asyncio.sleep(1)
-    with open("../raw-data/classes.json", "w") as f:
-        f.write(json.dumps(classes))
 
+    nextKey = max(newClasses, key='key')
+    for entry in rawClasses:
+        entry["key"] = -nextKey
+        entry["allInGroup"]['key'] = -nextKey
+        newClasses.append(entry)
+    
+    with open("../raw-data/rawClasses.json", "w") as f:
+        json.dump(newClasses, f, indent=4)
 
 if __name__ == "__main__":
     asyncio.run(main())
